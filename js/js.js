a84c1b12085b2f23ff591540a993dc24e74129cd
Taylor Welling <tw3080@gmail.com>
2016-05-19 20:51:57 -0700

    added functions to add/remove list items

--- js/js.js
+++ js/js.js
@@ -1,3 +1,17 @@
+function addListItem() {
+  var text = $("#new-item").val();
+  $("#list-tab").append('<li>'+text+'<i class="fa fa-times delete" aria-hidden="true"></i><i class="fa fa-check added" aria-hidden="true"></i></li>');
+  $("#new-item").val('');
+}
+
+function deleteItem() {
+  $(this).parent().remove();
+}
+
+function addToCart() {
+  $(this).parent().css('textDecoration', 'line-through');
+}
+
 $(document).ready(function() {
   $('.tabs .tab-links a').on('click', function(e)  {
     var currentAttrValue = $(this).attr('href');
@@ -9,5 +23,9 @@ $(document).ready(function() {
     $(this).parent('li').addClass('active').siblings().removeClass('active');
 
     e.preventDefault();
-  });
+  })
+
+  $("#add").on('click', addListItem);
+  $(document).on('click', '.delete', deleteItem);
+  $(document).on('click', '.added', addToCart);
 });
