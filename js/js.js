d7a97a5773b3bc6d568d70df46f1112c7084c25c
Taylor Welling <tw3080@gmail.com>
2016-05-20 01:01:34 -0700

    added transition animations

--- js/js.js
+++ js/js.js
@@ -1,31 +1,42 @@
 function addListItem() {
   var text = $("#new-item").val();
-  $("#list-tab").append('<li>'+text+'<i class="fa fa-times delete" aria-hidden="true"></i><i class="fa fa-check added" aria-hidden="true"></i></li>');
-  $("#new-item").val('');
+  if (text.length > 0) {
+    $("#list-tab").append('<li>'+text+'<i class="fa fa-times delete" aria-hidden="true"></i><i class="fa fa-check added" aria-hidden="true"></i></li>');
+    $("#new-item").val('');
+    $(".tab-content #list-p").hide();
+  }
 }
 
 function deleteItem() {
-  $(this).parent().remove();
+  $(this).parent().fadeOut();
 }
 
 function addToCart() {
-  $(this).parent().css('textDecoration', 'line-through');
+  $(this).parent().appendTo('#cart-tab');
+  $(this).hide();
+  $(".tab-content #cart-tab p").hide();
 }
+
+/* function addToCart() {
+  $(this).parent().fadeOut(function() {
+    $(this).parent().appendTo('#cart-tab');
+    $(this).hide();
+    $(".tab-content #cart-tab p").hide();
+  })
+} */
 
 $(document).ready(function() {
   $('.tabs .tab-links a').on('click', function(e)  {
     var currentAttrValue = $(this).attr('href');
-
     // Show/hide tabs
     $('.tabs ' + currentAttrValue).show().siblings().hide();
-
     // Change/remove current tab to active
     $(this).parent('li').addClass('active').siblings().removeClass('active');
-
     e.preventDefault();
   })
 
-  $("#add").on('click', addListItem);
+  $(document).on('click', '#add', addListItem);
+  /* $("#add").on('click', addListItem); */
   $(document).on('click', '.delete', deleteItem);
   $(document).on('click', '.added', addToCart);
 });
